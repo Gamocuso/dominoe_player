07146a28ac4e3133860c321f8c5567ebee74ce08
Gamocuso <gamocuso@gmail.com>
2024-06-15 18:29:05 -0300

    Estratégia txt e block play
    mudei a forma com que ele pega a peça que contém o maior núemro (começa do maior para o menor) e adicionei o block play

--- MC_102-main/dominoes/student_players.py
+++ MC_102-main/dominoes/student_players.py
@@ -48,11 +48,19 @@ class GuiPLayer(Player):
             return nones
 
         def play_mode(player, nones, poss_tiles, board_extremes):
+            """
+            Baseado no player, a função retorna as possíveis jogadas
+            podendo elas serem defensivas ou ofesivas dado o mesmo parâmetro.
 
+            --> jogadas defensivas: não prejudicam o amigo
+
+            --> ofensivas: buscam prejudicar o inimigo
+            """
             left = board_extremes[0]
             right = board_extremes[1]
 
             plays = []
+            blocks = []
 
             if player == "TEAM":
 
@@ -72,11 +80,24 @@ class GuiPLayer(Player):
                         if first not in nones:
                             plays.append([1, i])
             
-            else:
+            else:   
 
                 for i in poss_tiles:
                     first = i[0]
                     second = i[1]
+
+                    if left in nones:
+                        if first == right and second in nones:
+                            blocks.append([1, i])
+                        elif second == right and first in nones:
+                            blocks.append([1, i])
+
+                    if right in nones:
+                        if first == left and second in nones:
+                            blocks.append([0, i])
+                        elif second == left and first in nones:
+                            blocks.append([0, i])
+
                     if first == left:
                         if second in nones:
                             plays.append([0, i])
@@ -89,13 +110,23 @@ class GuiPLayer(Player):
                     if second == right:
                         if first in nones:
                             plays.append([1, i])
+                
+                if len(blocks) > 0:
+                    print("BLOCK PLAAY!!")
+                    return blocks
 
             return plays
 
         def t_sum(tile):
+            """
+            Retorna o valor total da peça
+            """
             return tile[0] + tile[1]
 
         def organize_tiles(tiles):
+            """
+            Organiza as peças em ordem crescente utilizando o quickSort
+            """
 
             def quick_sort(tile_list):
 
@@ -127,6 +158,11 @@ class GuiPLayer(Player):
 
     
         def count_hand():
+            """
+            Conta as peças na mão e retorna a peça
+            de maior ocorrência
+            """
+
             better_number = -1
             better_quantity = 0
             hand_tiles = self._tiles
@@ -147,6 +183,10 @@ class GuiPLayer(Player):
             return better_number, better_quantity
         
         def best_play (number, hand, extremes):
+            """
+            Sequência de estratégias que retornam a melhor jogada
+            baseado na ordem de prioridade adotada
+            """
 
             if len(extremes) > 1 and len(hand) > 1:
 
@@ -165,25 +205,32 @@ class GuiPLayer(Player):
 
                     plays = []
 
-                    if len(defensive_plays) > 0:
-                        plays += defensive_plays
                     if len(offensive_plays) > 0:
-                        plays += offensive_plays
+                        offensive_plays = organize_tiles(offensive_plays)
+                        double_offensive = find_double(offensive_plays, number) 
+                        if double_offensive:
+                            return double_offensive[0], double_offensive[1]
+                        else:
+                            return offensive_plays[-1][0], offensive_plays[-1][1]
 
-                    if len(plays) > 0:
-                        plays = organize_tiles(plays)
-                        double_play = find_double(plays, number)
-                        play = plays[-1] if not double_play else double_play
-                        return play[0], play[1]
+                    if len(defensive_plays) > 0:
+                        defensive_plays = organize_tiles(defensive_plays)
+                        double_defensive = find_double(defensive_plays, number) 
+                        if double_defensive:
+                            return double_defensive[0], double_defensive[1]
+                        else:
+                            return defensive_plays[-1][0], defensive_plays[-1][1]
 
                 hand = organize_tiles(hand)
                 double_hand = find_double(hand, number)
+
                 if double_hand:
                     return double_hand[0], double_hand[1]
+                    
                 left = extremes[0]
                 right = extremes[1]
 
-                for tile in hand:
+                for tile in hand[-1::-1]:
                     if tile[1][0] == number:
                         if tile[1][1] == left:
                             return tile[0], tile[1]
@@ -208,158 +255,16 @@ class GuiPLayer(Player):
             return 1, None
 
         playable_tiles = [tile for tile in self._tiles if tile[0] in board_extremes or tile[1] in board_extremes]
-        print(playable_tiles)
         hand_tiles = self._tiles
         if len(board_extremes) < 1:
             return 1, hand_tiles[0]
         if len(playable_tiles) == 1:
             return 1, playable_tiles[0]
         number_ , quantity = count_hand()
-        print(number_)
         
         best_play = best_play(number_, playable_tiles, board_extremes)
 
         return best_play
-
-class MoreiraPlayer(Player):
-    
-    def __init__(self):
-        super().__init__(2, "Moreira")
-    
-    def generate(self):
-        dominoes = [[] for _ in range(10)]
-        for i in range(10):
-            for j in range(10):
-                dominoes[i].append((i, j))
-        return dominoes
-
-    def play(self, board_extremes, play_hist):
-
-        def probabilitie(play_hist, poss_tiles, board_extremes):
-
-            p_enemie = play_hist[-3][0]
-            enemie_plays = [i for i in play_hist if i[0] == p_enemie]
-            enemie_plays = enemie_plays[-1::-1] # inverter para pegar o histórico em seu seguimento
-
-            nones = set()
-
-            for play in enemie_plays:
-                if play[-1] == None and len(play[1]) > 0:
-                    nones.add(play[1][0])
-                    nones.add(play[1][1])
-
-            left = board_extremes[0]
-            right = board_extremes[1]
-
-            plays = []
-
-            for i in poss_tiles:
-                first = i[0]
-                second = i[1]
-                if first == left:
-                    if second in nones:
-                        plays.append((0, i))
-                if first == right:
-                    if second in nones:
-                        plays.append((1, i))
-                if second == left:
-                    if first in nones:
-                        plays.append((0, i))
-                if second == right:
-                    if first in nones:
-                        plays.append((1, i))
-
-            return plays
-
-        def count_hand():
-            better_number = -1
-            better_quantity = 0
-            hand_tiles = self._tiles
-            for x in range(10):
-                contagem = 0
-                for tile in hand_tiles:
-                    if tile[0] == x and tile[1] != x:
-                        contagem += 1
-                    elif tile[1] == x and tile[0] != x:
-                        contagem += 1
-                    elif tile[0] == x and tile[1] == x:
-                        contagem += 1
-                if contagem > better_number:
-                    better_number = x
-                    better_quantity = contagem
-                return better_number, better_quantity
-        
-        def best_play (number, hand, extremes):
-
-            if len(extremes) > 1:
-                if len(play_hist) > 2:
-                    agressive_play_ = probabilitie(play_hist, hand, extremes)
-
-                    if len(agressive_play_) > 0:
-                        better = [0, [0]]
-
-                        for play in agressive_play_:
-                            side = play[0]
-                            tile = play[1]
-                            if sum(tile) > sum(better[1]):
-                                better = [side, tile]
-                        
-                        return better[0], better[1]
-
-                for tile in hand:
-                    if tile[0] == number:
-                        if tile[1] == extremes[0]:
-                            return 0 , tile
-                        elif tile[1] == extremes[1]:
-                            return 1 , tile
-                    elif tile[1] == number:
-                        if tile[0] == extremes[0]:
-                            return 0 , tile
-                        elif tile[0] == extremes[1]:
-                            return 1 , tile
-            return None
-
-        playable_tiles = [tile for tile in self._tiles if tile[0] in board_extremes or tile[1] in board_extremes]
-        hand_tiles = self._tiles
-        if len(board_extremes) < 1 and len(playable_tiles) > 1:
-            return playable_tiles[0]
-        number_ , quantity = count_hand()
-        
-        if best_play(number_, playable_tiles, board_extremes) != None:
-            return best_play(number_, playable_tiles, board_extremes)
-        else:
-            bigger = -1
-            tile = -1
-            for x in playable_tiles:
-                if (int(x[0]) + int(x[1])) > bigger:
-                    bigger = int(x[0]) + int(x[1])
-                    tile = x
-            if tile != -1:
-                if board_extremes[0] == tile[0] or board_extremes[0] == tile[1]:
-                    return 1, tile
-                else:
-                    return 2, tile
-        return 1, None
-
-class fdpPlayer(Player):
-
-    def __init__(self):
-        super().__init__(2, "Moreira")
-
-    def play(self, board_extremes, play_hist):
-        playable_tiles = self._tiles
-        if len(board_extremes) > 0:
-            playable_tiles = [tile for tile in self._tiles if tile[0] in board_extremes or tile[1] in board_extremes]
-        highest = -1
-        tile_sum = -1
-        for i in range(len(playable_tiles)):
-            if playable_tiles[i][0] + playable_tiles[i][1] > tile_sum:
-                tile_sum = playable_tiles[i][0] + playable_tiles[i][1]
-                highest = i
-        if highest >= 0:
-            return 1, playable_tiles[highest]
-        else:
-            return 1, None
 
 # Função que define o nome da dupla:
 def pair_name():
